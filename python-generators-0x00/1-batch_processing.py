2b9f676b4027e2dc20f1464221f9a91d3bfb1079
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2025-07-13 23:34:08 +0300

    Third Commit

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -3,6 +3,24 @@ processing = __import__('1-batch_processing')
 
 ##### print processed users in a batch of 50
 def stream_users_in_batches(batch_size):
+    def fetch_users():
+    # Connect to the database (change 'your_database.db' to your file)
+        conn = sqlite3.connect('your_database.db')
+        cursor = conn.cursor()
+
+    # Write the query
+        query = "SELECT * FROM user_data"
+    
+    # Execute the query
+        cursor.execute(query)
+
+    # Fetch all rows
+        rows = cursor.fetchall()
+
+    # Close the connection
+        conn.close()
+
+        return rows
     """Yield all users one by one (stream)"""
     for user in processing.batch_processing(batch_size):
         yield user  # loop #1
