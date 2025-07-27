ef0acc87371ebeaa444f2985369d31712d9b1c64
Mahmoud Ali <mahmoudaliy1492@gmail.com>
2025-07-27 23:48:32 +0300

    Tenth Commit

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
+from rest_framework import routers
 from rest_framework.routers import DefaultRouter  # ✅ Now included
 
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()  # ✅ Now declared
+router = routers.DefaultRouter()  # ✅ Now declared
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
